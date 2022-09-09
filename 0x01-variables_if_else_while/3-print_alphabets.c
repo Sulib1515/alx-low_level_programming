f95f854760d4ced7890a137ab29f536d41ceae8b
Suleiman Ibrahim <suleimanibrahimboy2018@gmail.com>
2022-09-09 14:27:16 -0700

    please nah

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,19 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-	for lowh = 'A'; low <= 'Z'; low++)
+	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	putchat(\n);
+
 	return (0);
 }
