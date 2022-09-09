e0ab21732dba76494a44fa68907ec051cb8737fc
Suleiman Ibrahim <suleimanibrahimboy2018@gmail.com>
2022-09-09 09:49:41 -0700

    fix na

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry ppoint
@@ -12,7 +14,7 @@ int main(void);
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar('\n);
+		putchar('\n');
 
 		return (0);
 }
