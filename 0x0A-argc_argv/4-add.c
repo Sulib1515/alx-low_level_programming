46f2abd1bfad7c052264827460bf6c58f726c2e1
Suleiman Ibrahim <suleimanibrahimboy2018@gmail.com>
2022-09-30 15:10:12 -0700

    argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - A program that adds positive numbers
+ * @argc: The arguments' counter
+ * @argv: The argument's values
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,11 @@
+Task0- Write a program that prints its name, followed by a new line.
+
+Task1- Write a program that prints the number of arguments passed into it.
+
+Task2- Write a program that prints all arguments it receives.
+
+Task3- Write a program that multiplies two numbers.
+
+Task4- Write a program that adds positive numbers.
+
+Task5- Write a program that prints the minimum number of coins to make change for an amount of money.
