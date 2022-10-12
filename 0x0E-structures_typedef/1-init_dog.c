db0443b9fba517f951921bbb9183f713f2cf0c41
Suleiman Ibrahim <suleimanibrahimboy2018@gmail.com>
2022-10-11 19:29:36 -0700

    err

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,10 @@
  */
 oid init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
